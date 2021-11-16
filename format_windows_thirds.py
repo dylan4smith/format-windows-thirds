dcbf4ccb2cf59148abb88b5853cc5558b0d5d1c5
Dylan Smith <50001455+dylan4smith@users.noreply.github.com>
2021-11-16 15:45:29 -0800

    Update format_windows_thirds.py

--- format_windows_thirds.py
+++ format_windows_thirds.py
@@ -1,4 +1,4 @@
-#          Copyright Joe Coder 2004 - 2006.
+#          Copyright Dylan Smith 2021.
 # Distributed under the Boost Software License, Version 1.0.
 #    (See accompanying file LICENSE_1_0.txt or copy at
 #          https://www.boost.org/LICENSE_1_0.txt)
